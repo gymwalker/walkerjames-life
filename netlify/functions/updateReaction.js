5a23e43c7f93028ceaeb318729ea43c51bf56e94
gymwalker <jimwalker29@gmail.com>
2025-04-17 20:05:19 -0700

    Update updateReaction.js

--- netlify/functions/updateReaction.js
+++ netlify/functions/updateReaction.js
@@ -1,1 +1,29 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function (event, context) {
+  const { recordId, field } = JSON.parse(event.body);
+  const airtableToken = process.env.AIRTABLE_TOKEN;
+  const baseId = "appaA8MFWiiWjXwSQ";
+  const tableName = "Letters";
+
+  const url = `https://api.airtable.com/v0/${baseId}/${tableName}/${recordId}`;
+
+  const response = await fetch(url, {
+    method: "PATCH",
+    headers: {
+      Authorization: `Bearer ${airtableToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      fields: { [field]: 1 }
+    }),
+  });
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
 
