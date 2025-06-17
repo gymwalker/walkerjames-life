4e01a0b2b25ff3a922ad255fe4219f828c0e1493
gymwalker <jimwalker29@gmail.com>
2025-06-17 13:37:26 -0700

    Update updateReaction.js

--- public/ltg-wall/updateReaction.js
+++ public/ltg-wall/updateReaction.js
@@ -1,7 +1,7 @@
 // Netlify Function: updateReaction.js
 const Airtable = require("airtable");
 
-const base = new Airtable({ apiKey: process.env.AIRTABLE_TOKEN }).base("appaA8MFWiiWjXwSQ");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_TOKEN }).base("pat0rNlezikkjBKM4.1e9161d0ad2f7c619af8d74ad1982f2aa752fdd90b6a768687912a40b41751a8");
 const tableName = "Letters";
 
 exports.handler = async (event) => {
