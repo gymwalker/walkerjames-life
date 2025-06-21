9afb437fde31e7f7e5ee90d50be7b26d9bbdec5b
gymwalker <jimwalker29@gmail.com>
2025-06-21 12:46:02 -0700

    Update getLetters.js

--- netlify/functions/getLetters.js
+++ netlify/functions/getLetters.js
@@ -2,12 +2,15 @@ const axios = require("axios");
 
 exports.handler = async (event, context) => {
   try {
+    console.log("Sending request to Make webhook...");
     const response = await axios.post("https://hook.us2.make.com/sp9n176bkb7uzawj5uj7255w9ljznth");
+
+    console.log("Received response:", response.data);
 
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "*", // You can replace * with https://walkerjames.life for security
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         "Content-Type": "application/json",
@@ -15,7 +18,8 @@ exports.handler = async (event, context) => {
       body: JSON.stringify(response.data),
     };
   } catch (error) {
-    console.error("Error fetching letters:", error.message);
+    console.error("Error fetching letters from Make:", error.message);
+    console.error("Full error object:", JSON.stringify(error, null, 2));
 
     return {
       statusCode: 500,
